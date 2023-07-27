315e35667956b043bc7eb667684f57b092c2ffc4
Maxim <mak.dragun2012@yandex.ru>
2023-07-27 15:23:48 +0300

    cataas.com went down entering warnings on the operation of the function

--- central-service/src/main/java/org/example/services/strategyBotCommands/impl/BotCommandStrategyCat.java
+++ central-service/src/main/java/org/example/services/strategyBotCommands/impl/BotCommandStrategyCat.java
@@ -3,9 +3,8 @@ package org.example.services.strategyBotCommands.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.enums.BotCommands;
 import org.example.models.ApplicationUser;
-import org.example.services.interfaces.CatPictureService;
 import org.example.services.strategyBotCommands.interfaces.BotCommandStrategy;
-import org.example.util.interfaces.SendPhotoUtil;
+import org.example.util.interfaces.SendMessageUtil;
 import org.springframework.stereotype.Service;
 
 import static org.example.enums.BotCommands.CAT_PICTURE;
@@ -13,12 +12,15 @@ import static org.example.enums.BotCommands.CAT_PICTURE;
 @RequiredArgsConstructor
 @Service
 public class BotCommandStrategyCat implements BotCommandStrategy {
-    private final CatPictureService catPictureService;
-    private final SendPhotoUtil sendPhotoUtil;
+    //    private final CatPictureService catPictureService;
+//    private final SendPhotoUtil sendPhotoUtil;
+    private final SendMessageUtil sendMessageUtil;
 
     @Override
     public void sendAnswer(ApplicationUser applicationUser, long chatId) {
-        sendPhotoUtil.sendAnswerForCatInline(catPictureService.getLinkCatPicture(), chatId);
+//        sendPhotoUtil.sendAnswerForCatInline(catPictureService.getLinkCatPicture(), chatId);
+        sendMessageUtil.sendAnswerDefault("В данный момент функция не работает!\n" +
+                "Попробуйте позже!", chatId);
     }
 
     @Override
